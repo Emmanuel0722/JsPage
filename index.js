ffd8c635808d93d6dc6f7fde3d602416567e6e16
Emmanuel <Emmanuel_0790@outlook.com>
2024-03-10 15:59:32 -0400

    Agregando algunos campos...

--- index.js
+++ index.js
@@ -1,16 +1,77 @@
 const hamburguer = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu-navegacion");
+const jsContainer = document.getElementById("jsContainer");
+const javaScript = document.getElementById("javaScript");
+// const contenedor = document.getElementById("contenedor");
+
+//obtiene elementos
+// const contenedor = document.querySelector('.dropdown .title');
+
+//vincula listeners a estos elementos
+
+// const contenedorParent = javaScript.parentElement;
+
+javaScript.addEventListener("click", toggleMenuDisplay);
+
+function toggleClass(elem, className) {
+  if (elem.className.indexOf(className) !== -1) {
+    elem.className = elem.className.replace(className, "");
+  } else {
+    elem.className = elem.className.replace(/\s+/g, " ") + " " + className;
+  }
+
+  return elem;
+}
+
+function toggleDisplay(elem) {
+  const curDisplayStyle = elem.style.display;
+
+  if (curDisplayStyle === "none" || curDisplayStyle === "") {
+    elem.style.display = "block";
+  } else {
+    elem.style.display = "none";
+  }
+}
+
+function toggleMenuDisplay(e) {
+  const dropdown = e.currentTarget.parentNode;
+  const contenedorGaleria = dropdown.querySelector(".contenedor-galeria");
+
+  toggleClass(contenedorGaleria, "hide");
+}
 
 // console.log(menu);
 // console.log(hamburguer);
 
-hamburguer.addEventListener('click', ()=>{
+hamburguer.addEventListener("click", () => {
+  menu.classList.toggle("spread");
+});
+
+window.addEventListener("click", (e) => {
+  if (
+    menu.classList.contains("spread") &&
+    e.target != menu &&
+    e.target != hamburguer
+  ) {
     menu.classList.toggle("spread");
+  }
 });
 
-window.addEventListener('click', (e)=>{
-    if (menu.classList.contains('spread') && e.target != menu && e.target != hamburguer) {
-      menu.classList.toggle("spread");
-    }
-});
+// function toggleClass(elemet, className) {}
 
+// javaScript.addEventListener("click", () => {
+//   // CAMBIAR LA CLASE DEL CONTENEDOR...
+//   jsContainer.classList.toggle("hide");
+//   if (jsContainer.className == "contenedor-galeria hide") {
+//     setTimeout(() => {
+//       jsContainer.parentElement.classList.toggle("hideSecond");
+//       jsContainer.classList.remove("hide");
+//     }, 1500);
+//   } else {
+//     setTimeout(() => {
+//       jsContainer.parentElement.classList.toggle("contenedor-galeria");
+//       jsContainer.classList.remove("hide");
+//       console.log("Abajo");
+//     }, 500);
+//   }
+// });
--- lightbox.js
+++ lightbox.js
@@ -1,12 +1,8 @@
-const imagenes = document.querySelectorAll('.img-galeria');
-const imagenesLight = document.querySelector('.agregar-imagen');
-const contenedorLight = document.querySelector('.imagen-light');
+const imagenes = document.querySelectorAll(".img-galeria");
+const imagenesLight = document.querySelector(".agregar-imagen");
+const contenedorLight = document.querySelector(".imagen-light");
 const hamburguer1 = document.querySelector(".hamburger");
-const aPagina = document.getElementById('aPagina');
-
-// console.log(imagenes);
-// console.log(imagenesLight);
-// console.log(contenedorLight);    "./imagenes/fakeGoogle.PNG":
+const aPagina = document.getElementById("aPagina");
 
 const aEnlace = [
   "https://emmanuel0722.github.io/JsSearch/",
@@ -14,49 +10,33 @@ const aEnlace = [
   "https://emmanuel0722.github.io/JsProduct/",
   "https://emmanuel0722.github.io/JsSitiosFav/",
   "https://emmanuel0722.github.io/JsPokedex/",
-  "https://emmanuel0722.github.io/JsYesNop/"
-]
+  "https://emmanuel0722.github.io/JsYesNop/",
+];
 
-// console.log(aEnlace["./imagenes/fakeGoogle.PNG"])
-// aEnlace.forEach(enlace => {
-//   console.log(enlace);
-// })
-
-// const aEnlace = [
-//   "https://emmanuel0722.github.io/fakeGoogle/",
-//   "https://emmanuel0722.github.io/JsTasks/",
-//   "Nada por Aquí.",
-//   "Nada por acá.",
-//   "Nara por acuyá.",
-//   "Nada de Nada."
-// ]
-// console.log(aEnlace);
-// console.log(aEnlace1);
-
-imagenes.forEach(imagen => {
-  imagen.addEventListener('click', ()=>{
-    aparecerImagen(imagen.getAttribute('src'), imagen.getAttribute('alt'));
-  })
+imagenes.forEach((imagen) => {
+  imagen.addEventListener("click", () => {
+    aparecerImagen(imagen.getAttribute("src"), imagen.getAttribute("alt"));
+  });
 });
 
-contenedorLight.addEventListener('click', (e) => {
-    if (e.target !== imagenesLight) {
-      contenedorLight.classList.toggle('show');
-      imagenesLight.classList.toggle('showImagen');
-      hamburguer1.style.opacity = '1';
-    }
-})
+contenedorLight.addEventListener("click", (e) => {
+  if (e.target !== imagenesLight) {
+    contenedorLight.classList.toggle("show");
+    imagenesLight.classList.toggle("showImagen");
+    hamburguer1.style.opacity = "1";
+  }
+});
 
 const aparecerImagen = (imagen, link) => {
   imagenesLight.src = imagen;
 
-  contenedorLight.classList.toggle('show');
-  imagenesLight.classList.toggle('showImagen');
-  hamburguer1.style.opacity = '0';
+  contenedorLight.classList.toggle("show");
+  imagenesLight.classList.toggle("showImagen");
+  hamburguer1.style.opacity = "0";
 
   for (let i = 0; i < aEnlace.length; i++) {
     if (aEnlace[i] == link) {
-      aPagina.href = link
+      aPagina.href = link;
     }
   }
-}
+};
